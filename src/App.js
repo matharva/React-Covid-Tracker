d9d3c5efe3c56c4b5abab29d79063de151ecb102
Atharva Mohite <atharvamohite20.com>
2021-05-11 23:23:23 +0530

    Dropdown Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import "./App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+    console.log(countries);
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="App">
-      <h1>Covid Tracker</h1>
+      <div className="app__header">
+        <h1>Covid Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>;
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name} </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title + Dropdown */}
